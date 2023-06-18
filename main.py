aeb6940541bb57524c6d3b287cbfc5b1a8c94bb8
ENEOJO111 <raphael.idakwo@binghamuni.edu.ng>
2023-06-18 01:43:40 -0700

    first commit

--- main.py
+++ main.py
@@ -1,34 +1,109 @@
-import tkinter as tk
-from tkinter import messagebox, simpledialog
+
+class Maths:
+    @staticmethod
+    def addition(a, b):
+        return a + b
+
+    @staticmethod
+    def subtraction(a, b):
+        return a - b
+
+    @staticmethod
+    def multiplication(a, b):
+        return a * b
+
+    @staticmethod
+    def division(a, b):
+        return a / b
+
+    @staticmethod
+    def power(a, b):
+        return a ** b
 
 
+class Physics:
+    @staticmethod
+    def velocity(distance, time):
+        return distance / time
 
-def write(master, name, oc, pin):
-    if ((is_number(name)) or (is_number(oc) == 0) or (is_number(pin) == 0) or name == ""):
-        messagebox.showinfo("Error", "Invalid Credentials\nPlease try again.")
-        master.destroy()
-        return
+    @staticmethod
+    def acceleration(velocity, time):
+        return velocity / time
 
-    f1 = open("Account_Record.txt", 'r')
-    accnt_no = int(f1.readline())
-    accnt_no += 1
-    f1.close()
+    @staticmethod
+    def force(mass, acceleration):
+        return mass * acceleration
 
-    f1 = open("Account_Record.txt", 'w')
-    f1.write(str(accnt_no))
-    f1.close()
+    @staticmethod
+    def energy(mass, velocity):
+        return 0.5 * mass * (velocity ** 2)
 
-    fdet = open(str(accnt_no) + ".txt", "w")
-    fdet.write(pin + "\n")
-    fdet.write(oc + "\n")
-frec.write("Date                             Credit      Debit     Balance\n")
-frec.write(str(strftime("[%Y-%m-%d] [%H:%M:%S]  ", gmtime())) + "     " + oc + "              " + oc + "\n")
-frec.close()
+    @staticmethod
+    def power(work, time):
+        return work / time
 
-messagebox.showinfo("Details", "Your Account Number is:" + str(accnt_no))
-master.destroy()
-return value
 
-app= BankApp()
-tk.mainloop()
+def main():
+    while True:
+        subject = input("Which subject do you want to calculate: 'maths' or 'physics'? ").lower()
+
+        if subject == "maths":
+            operation = input("Choose an operation: 'addition', 'subtraction', 'multiplication', 'division', 'power': ")
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+
+            if operation == "addition":
+                result = Maths.addition(num1, num2)
+            elif operation == "subtraction":
+                result = Maths.subtraction(num1, num2)
+            elif operation == "multiplication":
+                result = Maths.multiplication(num1, num2)
+            elif operation == "division":
+                result = Maths.division(num1, num2)
+            elif operation == "power":
+                result = Maths.power(num1, num2)
+            else:
+                print("Invalid operation!")
+                continue
+
+        elif subject == "physics":
+            operation = input("Choose an operation: 'velocity', 'acceleration', 'force', 'energy', 'power': ")
+            if operation == "velocity":
+                distance = float(input("Enter the distance: "))
+                time = float(input("Enter the time: "))
+                result = Physics.velocity(distance, time)
+            elif operation == "acceleration":
+                velocity = float(input("Enter the velocity: "))
+                time = float(input("Enter the time: "))
+                result = Physics.acceleration(velocity, time)
+            elif operation == "force":
+                mass = float(input("Enter the mass: "))
+                acceleration = float(input("Enter the acceleration: "))
+                result = Physics.force(mass, acceleration)
+            elif operation == "energy":
+                mass = float(input("Enter the mass: "))
+                velocity = float(input("Enter the velocity: "))
+                result = Physics.energy(mass, velocity)
+            elif operation == "power":
+                work = float(input("Enter the work: "))
+                time = float(input("Enter the time: "))
+                result = Physics.power(work, time)
+            else:
+                print("Invalid operation!")
+                continue
+
+        else:
+            print("Invalid subject!")
+            continue
+
+        print(f"The result is: {result}\n")
+
+        repeat = input("Do you want to perform another calculation? (yes/no): ").lower()
+        if repeat != "yes":
+            break
+
+
+if __name__ == "__main__":
+    main()
+
 
